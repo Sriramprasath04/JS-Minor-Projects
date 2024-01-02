b41b879e6776c8cc34a73d4fad1f319e3d542056
Sriramprasath P <sriramprasath.2002@srit.org>
2024-01-02 10:46:37 +0530

    Add files via upload

---
+++ BMI Calculator/app.js
@@ -0,0 +1,7 @@
+var height = Number(prompt("Enter your height (int meter): "));
+var weight = Number(prompt("Enter your weight (in Kg): "));
+
+
+var hts = height*height
+var bmi = weight/hts;
+alert("BMI: "+bmi);
